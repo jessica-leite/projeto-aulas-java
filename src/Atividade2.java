2fe80b5bca69a05f62f05504ed6d368825828f43
Jessica Leite <jessica.mleite@gmail.com>
2024-03-11 16:56:49 -0300

    Metodo verificar numero inteiro

--- src/Atividade2.java
+++ src/Atividade2.java
@@ -2,6 +2,33 @@ import java.util.Scanner;
 
 public class Atividade2 {
 
+    public static  void verificarNumeroInteiro(){
+        //5.Escreva um programa em Java que solicita ao usuário um número inteiro.
+        System.out.println("Digite um número inteiro: ");
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+
+        // Com base nesse número, o programa deve realizar as seguintes ações:
+        //1. Se o número for positivo, armazene-o em uma variável e exiba uma mensagem indicando que é positivo.
+        if (numero > 0){
+            System.out.println("O número " + numero + " é positivo!");
+        }
+        //2. Se o número for negativo, armazene o valor absoluto em uma variável e exiba uma mensagem indicando que é negativo.
+        if(numero < 0){
+            System.out.println("O número " + numero + " é negativo!");
+        }
+        //3. Utilize um bloco switch para verificar se o número é par ou ímpar. Exiba uma mensagem correspondente.
+        switch (numero%2){
+            case 0:
+                System.out.println("O número é par!");
+                break;
+            case 1:
+            case -1:
+                System.out.println("O número é ímpar!");
+                break;
+        }
+    }
+
     public static void simularPedidos() {
         //6 -Desenvolva um programa em Java para simular um sistema de pedidos de uma loja online. O programa deve:
         // 1. Solicitar ao usuário que insira o valor total de sua compra.
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Atividade2.simularPedidos();
+        Atividade2.verificarNumeroInteiro();
     }
 
     public static void verificarDiaSemana(){
