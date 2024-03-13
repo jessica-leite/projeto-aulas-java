374225b9ed95f2b6fbbf375fd4961896e73a5871
Jessica Leite <jessica.mleite@gmail.com>
2024-03-12 21:54:04 -0300

    Ajuste mensagem

--- src/Main.java
+++ src/Main.java
@@ -46,7 +46,7 @@ public class Main {
                 System.out.println(i);
             }
         } else {
-            System.out.println("Número negativo!");
+            System.out.println("Número inválido!");
         }
 
     }
