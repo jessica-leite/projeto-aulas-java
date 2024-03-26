f63df71a1ad853cd889977320abccc4d22e39428
Jessica Leite <jessica.mleite@gmail.com>
2024-03-26 19:34:21 -0300

    Exercicio veiculos

---
+++ src/veiculos/Main.java
@@ -0,0 +1,21 @@
+package veiculos;
+
+public class Main {
+    //Suponha que você esteja criando um sistema para gerenciar diferentes tipos de veículos. Você pode ter uma classe básica chamada Veiculo e, em seguida, classes específicas que herdam dela, como Carro e Moto. Cada classe pode ter seus próprios métodos e atributos, além de herdar aqueles da classe Veiculo.
+    //
+    //classe veiculo
+    //- Atributos
+    //	marca
+    //	modelo
+    //
+    //classe Carro
+    //- Atributos
+    //	numero_portas
+    //	cor
+    //	ano
+    //
+    //classe Moto
+    //	cilindradas
+    //	cor
+    //	ano
+}
