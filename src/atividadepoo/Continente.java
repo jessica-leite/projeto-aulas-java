2948afed9ebd0b044428dc86d060584f4b6344aa
Jessica Leite <jessica.mleite@gmail.com>
2024-04-15 16:22:15 -0300

    Classe Continente

---
+++ src/atividadepoo/Continente.java
@@ -0,0 +1,67 @@
+package atividadepoo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Continente {
+    //2) Escreva em Java uma classe Continente. Um continente possui um nome e é composto
+    //por um conjunto de países. Forneça os membros de classe a seguir:
+    //a) Construtor que inicialize o nome do continente;
+    //b) Um método que permita adicionar países aos continentes;
+    //c) Um método que retorne a dimensão total do continente;
+    //d) Um método que retorne a população total do continente;
+    //e) Um método que retorne a densidade populacional do continente;
+    //f) Um método que retorne o país com maior população no continente;
+    //g) Um método que retorne o país com menor população no continente;
+    //h) Um método que retorne o país de maior dimensão territorial no continente;
+    //i) Um método que retorne o país de menor dimensão territorial no continente;
+    //j) Um método que retorne a razão territorial do maior pais em relação ao menor país.
+
+    private String Nome;
+    private double Dimensao;
+    private int Populacao;
+    private double DensidadePopulacional;
+
+    private List<Pais> Paises = new LinkedList<Pais>() {};
+
+    public Continente(String nome){
+     Nome = nome;
+    }
+
+    public void AdicionarPais(String nome){
+        Paises.add(new Pais(nome));
+    }
+
+    public double GetDimensao(){
+        return Dimensao;
+    }
+
+    public int GetPopulacao(){
+        return Populacao;
+    }
+
+    public double GetDensidadePopulacional(){
+        return DensidadePopulacional;
+    }
+
+    public Pais GetPaisMaiorPopulacao(){
+        return null;
+    }
+
+    public Pais GetPaisMenorPopulacao(){
+        return null;
+    }
+
+    public Pais GetPaisMaiorDimensaoTerritorial(){
+        return null;
+    }
+
+    public Pais GetPaisMenorDimensaoTerritorial(){
+        return null;
+    }
+
+    public double GetRazaoTerritorialMaiorEMenorPais(){
+        return 0;
+    }
+}
+
---
+++ src/atividadepoo/Pais.java
@@ -0,0 +1,11 @@
+package atividadepoo;
+
+public class Pais {
+    private String Nome;
+
+    public Pais(String nome){};
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+}
